2d2b254e115871f97c7037402fd9ee50e417dd89
Dobrosockiy Alexandr <dogstar.ru@gmail.com>
2026-04-09 22:23:48 +0300

    Афигеть я сделяль

---
+++ src/main/java/com/example/contentservice/parser/PdfParserService.java
@@ -0,0 +1,28 @@
+package com.example.contentservice.parser;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PdfParserService {
+
+    public List<BufferedImage> parse(MultipartFile file) throws IOException {
+        PDDocument document = PDDocument.load(file.getInputStream());
+        PDFRenderer renderer = new PDFRenderer(document);
+
+        List<BufferedImage> images = new ArrayList<>();
+        for (int i = 0; i < document.getNumberOfPages(); i++) {
+            images.add(renderer.renderImageWithDPI(i, 300));
+        }
+
+        document.close();
+        return images;
+    }
+}
