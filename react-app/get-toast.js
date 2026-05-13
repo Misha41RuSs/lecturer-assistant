4a86ed21451696d97a2ace4369f12c089255631f
Dobrosockiy Alexandr <dogstar.ru@gmail.com>
2026-05-13 22:58:03 +0300

    fix: удалён временный отладочный файл get-toast.js

--- react-app/get-toast.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = fs.readFileSync('node_modules/sonner/dist/index.mjs', 'utf8');
-const lines = file.split('\n');
-lines.forEach((l, i) => {
-  if (l.includes("React.createElement(\"li\"")) {
-     console.log(lines.slice(i, i+30).join('\n'));
-  }
-});
